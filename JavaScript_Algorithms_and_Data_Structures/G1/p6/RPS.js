755842338fc7f712c3c7bc51e2d8e8b00746b6e7
Yamlak Negash <yamlaknegash96@gmail.com>
2025-03-01 23:35:48 +0300

    Updated the JS file.

--- JavaScript_Algorithms_and_Data_Structures/G1/p6/RPS.js
+++ JavaScript_Algorithms_and_Data_Structures/G1/p6/RPS.js
@@ -3,3 +3,32 @@ function getRandomComputerResult() {
   const randomIndex = Math.floor(Math.random() * options.length);
   return options[randomIndex];
 }
+
+let playerScore = 0;
+let computerScore = 0;
+
+function hasPlayerWonTheRound(player, computer) {
+  return (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Scissors" && computer === "Paper") ||
+    (player === "Paper" && computer === "Rock")
+  );
+}
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+
+  if (hasPlayerWonTheRound(userOption, computerResult)) {
+    playerScore++;
+    return `Player wins! ${userOption} beats ${computerResult}`;
+  }
+
+  if (userOption === computerResult) {
+    return `It's a tie! Both chose ${userOption}`;
+  }
+
+  computerScore++;
+  return `Computer wins! ${computerResult} beats ${userOption}`;
+}
+
+console.log(getRoundResults("Rock"));
+console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
